7a1f2ecc9164edbf96352823567e75a55481afd3
Aritra Sarkar <aritra1911@yahoo.com>
2020-12-12 14:28:33 +0530

    Make ISR() check the monostable section of 555 Clock
    Also check for any bounces

--- 555_clock/555_clock.c
+++ 555_clock/555_clock.c
@@ -4,6 +4,9 @@
 #include "USART.h"
 #include "pinDefines.h"
 
+// MACROS
+#define TICKS_TO_MS(ticks) (ticks / 125)
+
 // CLOCK DIVISION DEFINES //////////////////
 #define DIV_1     (1 << CS10)
 #define DIV_8     (1 << CS11)
@@ -17,35 +20,59 @@
 #define SELECT PD3
 //////////////////
 
-// MISC DEFINES ///////////////////
-#define UPDATE_INTERVAL 5000  // ms
-///////////////////////////////////
+// MISC DEFINES ///////////////////////////////
+#define UPDATE_INTERVAL             5000  // ms
+#define THRESHOLD_DEBOUNCE_DURATION   80  // ms
+///////////////////////////////////////////////
 
-volatile uint16_t aHighTicks, aLowTicks, mTicks;
+volatile uint16_t highTicks, lowTicks;
 
-uint8_t calcDutyCycle(uint16_t highTicks, uint16_t lowTicks) {
-    uint8_t totalTicks = highTicks + lowTicks;
+uint8_t calcDutyCycle(uint8_t highTime, uint8_t lowTime) {
+    uint8_t timePeriod = highTime + lowTime;
 
-    if (!totalTicks)
+    if (!timePeriod)
         return 0;  // Avoid a division by zero
 
-    return ((uint32_t)highTicks * 100 / totalTicks);
+    return (highTime * 100) / timePeriod;
+}
+
+void monostableOut(uint16_t ticks) {
+    // Show ticks, duration of pulse and check if debounced!!
+    static uint8_t pulses = 0;
+
+    printString("\nMONOSTABLE MODE (Pulse #");
+    printByte(pulses++);
+    printStringLn(")");
+
+    printString("Ticks: ");
+    printWord(ticks);
+
+    uint8_t duration = TICKS_TO_MS(ticks);
+
+    printString("\nDuration: ");
+    printByte(duration);
+    printString(" ms");
+
+    if (duration < THRESHOLD_DEBOUNCE_DURATION)
+        printStringLn(" (Probably bounced)");
+    else
+        transmitByte('\n');
 }
 
 ISR(INT0_vect) {
     if (bit_is_set(PIND, SELECT)) {   // Astable operation
         if (bit_is_set(PIND, CLK)) {  // Rising edge: Get lowTicks and reset timer
-            aLowTicks = TCNT1;
+            lowTicks = TCNT1;
             TCNT1 = 0;
         } else {                      // Falling edge: Get highTicks and reset timer
-            aHighTicks = TCNT1;
+            highTicks = TCNT1;
             TCNT1 = 0;
         }
     } else {                          // Monostable operation
         if (bit_is_set(PIND, CLK))    // Rising edge: Reset the timer
             TCNT1 = 0;
         else                          // Falling edge: Get the count
-            mTicks = TCNT1;           // No need to reset the timer
+            monostableOut(TCNT1);     // No need to reset the timer
     }
 }
 
@@ -61,9 +88,8 @@ static inline void initTimer1(void) {
 int main(void) {
     LED_DDR = 0xff;         // Let's use the LEDs as status signal
 
-    aHighTicks = 0;
-    aLowTicks = 0;
-    mTicks = 0;
+    highTicks = 0;
+    lowTicks = 0;
 
     initUSART();
     initTimer1();
@@ -80,25 +106,16 @@ int main(void) {
             printStringLn("\nASTABLE MODE");
 
             printString("High Ticks: ");
-            printWord(aHighTicks);
+            printWord(highTicks);
 
             printString("\nLow Ticks: ");
-            printWord(aLowTicks);
+            printWord(lowTicks);
+            transmitByte('\n');
 
-            printByte(calcDutyCycle(aHighTicks, aLowTicks));  // TODO: Drop leading zeros
-            printStringLn(" %% duty cucle recorded");
+            printByte(calcDutyCycle(TICKS_TO_MS(highTicks), TICKS_TO_MS(lowTicks)));  // TODO: Drop leading zeros
+            printStringLn(" % duty cycle recorded");
 
-        } else {  // Monostable Mode: Show ticks and time elapsed during high time
-            printString("\nMONOSTABLE MODE");
-
-            printString("Ticks: ");
-            printByte(mTicks);
-
-            printString("\nDuration: ");
-            printByte(mTicks / 125);
-            printStringLn(" ms");
-
-        }
+        } // Monostable Mode is handled in it's own sub routine triggered by ISR()
     }
 
     return 0;
