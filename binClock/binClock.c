bd10974cba5b50375e68e2162f7eafb0166471f0
Aritra Sarkar <aritra1911@yahoo.com>
2021-01-23 18:55:42 +0530

    Enable interrupt drive for Binary Clock

--- binClock/binClock.c
+++ binClock/binClock.c
@@ -1,9 +1,27 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 uint8_t get_time(uint8_t);
+void tick(void);
 
 volatile uint8_t hours, minutes, seconds;
+volatile uint16_t millis;
+
+static inline void initTimer0(void) {
+    TCCR0A |= (1 << WGM01);               // Enable Timer0 in CTC Mode
+    TCCR0B |= (1 << CS01) | (1 << CS00);  // clk/64 (From prescaler)
+    TIMSK0 |= (1 << OCIE0A);              // Execute interrupt if timer value matches OCR0A value
+}
+
+ISR(TIMER0_COMPA_vect) {
+    millis++;
+
+    if (millis >= 1000) {
+        tick();
+        millis = 0;
+    }
+}
 
 void tick(void) {
     seconds++;
@@ -25,8 +43,15 @@ void tick(void) {
 int main(void) {
     uint8_t rc;
 
-    hours   = 0x0f;
-    minutes = 0x31;
+    OCR0A = 125;  // ISR() called at an interval of 1 millisecond since
+                  // 8 MHz / 64 = 125 ticks per millisecond
+    millis = 0;   // TODO: Can this be static inside the ISR()?
+
+    initTimer0();
+    sei();
+
+    hours   = 0x12;
+    minutes = 0x06;
     seconds = 0x00;
 
     DDRB = 0x3f;    // PB0-PB5 of PORTB are used as outputs
@@ -38,7 +63,7 @@ int main(void) {
 
     while (1) {
         // Cycle through all three banks of LEDs
-        _delay_ms(1);               // Wait a little while
+        _delay_ms(5);               // Wait a little while
 
         PORTB &= ~(0x3f);           // Clear 6 LSBs of PORTB
 
@@ -49,8 +74,6 @@ int main(void) {
             rc = 0x01;              // Move back to hour bank
         else
             rc <<= 1;               // Shift to next bank
-
-        tick();                     // The clock ticks
 
         PORTC |= rc;                // Turn on the proper LED Bank
         PORTB |= get_time(rc);      // Display the number belonging to that bank
